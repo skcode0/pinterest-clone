83c6e0cf25e4aadfa297e01f8ff021ddfa1faa4f
sk <sk.paradoxmaske@gmail.com>
2021-04-12 05:53:25 -0500

    install material-ui and styled-components; update resourced used in readme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import Nav from './components/Nav';
+import { GlobalStyle } from './App.style';
 
 function App() {
   return (
     <div className="App">
-      App
+      <GlobalStyle />
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PinterestIcon from '@material-ui/icons/Pinterest';
+
+function Header() {
+    return (
+        <nav>
+            <PinterestIcon />
+        </nav>
+    )
+}
+
+export default Header
